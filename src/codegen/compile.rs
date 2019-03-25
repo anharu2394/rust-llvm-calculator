ac7cc226b5b07d238914b9246843ed725ba0520b
Anharu <haruan2394@gmail.com>
2019-03-25 23:08:53 +0900

    Change: CompilationResult to Result

--- src/codegen/compile.rs
+++ src/codegen/compile.rs
@@ -1,6 +1,6 @@
 use crate::parser;
 use failure::Error;
 
-pub fn compile_string(source: &str) -> Result<CompilationResult, Error> {
+pub fn compile_string(source: &str) -> Result<Result, Error> {
     let ast = parser::parse(&source)?;
 }
