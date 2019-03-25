f42e2e109d85603d43ca482346b863d712c8545f
Anharu <haruan2394@gmail.com>
2019-03-25 17:00:01 +0900

    Add: define parse fn

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,16 @@
+use crate::ast::Node;
+use crate::error::ParseError;
 use failure::Error;
 
 pub mod syntax {
     include!(concat!(env!("OUT_DIR"), "/calculator.rs"));
+}
+
+pub fn parse(x: &str) -> Result<Node, Error> {
+    syntax::expr(x).map_err(|e| {
+        ParseError {
+            message: e.to_string(),
+        }
+        .into()
+    })
 }
