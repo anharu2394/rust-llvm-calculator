e7ce6a8ef96abc2ae98dffc81462c74b74e7f425
Anharu <haruan2394@gmail.com>
2019-03-26 14:55:33 +0900

    Add: receive input and parse input and compile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use anharu_calculator_project::codegen::compile::compile_string;
+use std::env::args;
 fn main() {
-    println!("Hello, world!");
+    let input = args().skip(1).next().expect("Failed to get first arg.");
+
+    println!("problem is {}", input);
+
+    match compile_string(&input).expect("Failed to compile.") {
+        Some(calc) => unsafe {
+            println!("The answer is {}.", calc.call());
+        },
+        None => println!("failed to get the llvm function."),
+    }
 }
